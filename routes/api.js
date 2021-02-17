fa3e58db9444e5030ab7f3f588df2a2c1e5b54a6
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-16 18:11:21 -0800

    database added

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,19 @@
-'use strict';
+"use strict";
+
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
 module.exports = function (app) {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+  let stockSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    ips: [String],
+  });
+
+  app.route("/api/stock-prices").get(function (req, res) {});
 };
