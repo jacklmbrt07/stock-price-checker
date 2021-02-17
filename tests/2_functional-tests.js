7587ae71a54e6b0f001016a3c1a357b683c19afc
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-16 22:24:06 -0800

    passes first test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,21 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
+suite("Functional Tests", function () {
+  test("1 stock", (done) => {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: "GOOG" })
+      .end((err, res) => {
+        assert.equal(res.body["stockData"]["stock"], "GOOG");
+        assert.isNotNull(res.body["stockData"]["price"]);
+        assert.isNotNull(res.body["stockData"]["likes"]);
+        done();
+      });
+  });
 });
